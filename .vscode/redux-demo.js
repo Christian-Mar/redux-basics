919edbca097b87b7a3b92ab762c5a468b6e03e7c
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-09-13 20:20:11 +0200

    Redux basic syntax

---
+++ .vscode/redux-demo.js
@@ -0,0 +1,26 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0}, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+  };
+}
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }  
+}
+
+const store = redux.createStore(counterReducer); 
+
+const counterSubsriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubsriber);
+
+store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'decrement'});
